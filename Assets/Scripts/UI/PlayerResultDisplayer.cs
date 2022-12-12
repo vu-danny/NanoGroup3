a1d7bb0186de1992452339589a2d4f0e969dde77
vu-danny <dan.vu.alx@gmail.com>
2022-12-12 17:01:05 +0100

    start implementing end screen

---
+++ Assets/Scripts/UI/PlayerResultDisplayer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerResultDisplayer : MonoBehaviour
+{
+    [SerializeField] Sprite bigBallIcon;
+    [SerializeField] Sprite speedIcon;
+    [SerializeField] Sprite blankIcon;
+
+    [SerializeField] Image ballImage;
+    [SerializeField] Image speedImage;
+    [SerializeField] TMP_Text playerText;
+    [SerializeField] TMP_Text timeText;
+    [SerializeField] TMP_Text sizeText;
+    
+    void Awake(){
+        InitializeValues(true, 69, 154.3521f, 24.2414f);
+        UpdateBall(true);
+    }
+    public void InitializeValues(bool first, int playerNumber, float timeSeconds, float size){
+        speedImage.sprite = first ? speedIcon : blankIcon;
+        playerText.text = "Joueur "+playerNumber;
+        int seconds = Mathf.FloorToInt(timeSeconds);
+        int ms = Mathf.FloorToInt((timeSeconds - seconds)*100);
+        int minutes = seconds/60;
+        seconds %= 60;
+        timeText.text = minutes.ToString("D2")+":"+seconds.ToString("D2")+":"+ms.ToString("D2");
+        sizeText.text = string.Format("{0:0.00}", size) +" m";
+    }
+
+    public void UpdateBall(bool biggest){
+        ballImage.sprite = biggest?bigBallIcon:blankIcon;
+    }
+}
