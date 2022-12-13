bec2f44f09e73ac6a2a938f12fbf1fc1c32623ff
Railnof <florianchevrin65@gmail.com>
2022-12-13 16:40:34 +0100

    Update ControllerAssignement.cs
    J'ai trouvé un code en ligne qui peut correspondre à l'assignation des manettes (premier qui clique est joueur 1 et ainsi de suite...) Mais je vois pas comment le modifier pour que ça marche

--- Assets/Scripts/ControllerAssignement.cs
+++ Assets/Scripts/ControllerAssignement.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControllerAssignation : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+// public class ControllerAssignation : MonoBehaviour
+// {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+    // void Awake()
+    // {
+        // DontDestroyOnLoad(gameObject);
+        // }
+            // private void Update()
+        // {
+            // if (SceneManager.GetActiveScene().name != "Menu" && !spawnedCars)
+            // {
+             // spawnedCars = true;
+             // int i = 0;
+             // while(i < playerCount)
+                // {
+                 // PlayerInput spawnedCar = PlayerInput.Instantiate(spawnPlayer, controlScheme: "Gamepad", pairWithDevice: Gamepad.all[i]);
+                 // spawnedCar.GetComponent<Car>().myPlayerNum = (i + 1);
+                 // foreach (string getTexture in avaterText)
+                 // {
+                     // if(getTexture.Contains("P" + (i + 1)))
+                     // {
+                         // SetTexture(spawnedCar.gameObject, getTexture, (i + 1));
+                     // }
+                 // }
+                 // i++;
+                // }
+            // }
+    // }
+// }
