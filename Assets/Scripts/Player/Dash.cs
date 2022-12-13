944c5e6183477cc2f1ea68796912b8b1499c4cd9
Lamaxelle <axelle.desquilbet@laposte.net>
2022-12-13 15:42:50 +0100

    dash scene created

---
+++ Assets/Scripts/Player/Dash.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+    public KeyCode dashKey;
+    public Vector3 initVelocity;
+    public float maxSpeedDash;
+    public float accelerationDuration;
+    public float decelerationDuration;
+    public SnowballSizer snowballSizer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DashByKey() 
+    { 
+    
+    }
+
+    public void DashByCollecting() 
+    { 
+        
+    }
+}
