5eee6fca582309d22d866b60399d6938f5e7cf4d
Pierrhum <34352456+Pierrhum@users.noreply.github.com>
2022-12-14 19:03:52 +0100

    Joystick feedback

---
+++ Assets/Scripts/UI/JoystickUI.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoystickUI : MonoBehaviour
+{
+    private Image Joystick;
+    private Vector2 InitJoystickPos;
+
+    private void Awake()
+    {
+        Joystick = GetComponent<Image>();
+        InitJoystickPos = Joystick.rectTransform.anchoredPosition;
+        Debug.Log(InitJoystickPos);
+    }
+
+    public void MoveJoystick(Vector2 gap)
+    {
+        Joystick.rectTransform.anchoredPosition = InitJoystickPos + new Vector2(Mathf.Clamp(gap.x, -10f, 10f), 
+            Mathf.Clamp(gap.y, -10f, 10f));
+    }
+}
